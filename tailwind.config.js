ffd15fe52008ae28e726c8262f2ca5f2c239d072
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2023-02-09 17:59:49 +0700

    Done-2

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main-bg': '#E4EFFF',
+        'text-color-dark': '#193766',
+        'primary-color': '#3B82F6',
+        'primary-light': '#C6DBFF',
+      },
+    },
+  },
+  plugins: [],
+}
